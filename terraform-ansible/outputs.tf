7601036cb754eea0b3396bd58dfae9e034505437
Vishal Mathur <vishal@Vishals-MacBook-Pro.local>
2023-05-27 22:50:22 +0530

    pushing the folder

---
+++ terraform-ansible/outputs.tf
@@ -0,0 +1,24 @@
+output "vpc_id" {
+  description = "ID of project VPC"
+  value       = aws_vpc.myvpc1.id
+}
+
+output "route_table" {
+  value = aws_route_table.myRT.id
+}
+
+output "subnet_id" {
+  description = "ID of subnet"
+  value       = aws_subnet.public1.id
+}
+
+output "security_groups" {
+  description = "ID of SG"
+  value       = aws_security_group.mywebsecurity.id
+}
+
+output "internet_gateway" {
+  value = aws_internet_gateway.myIGW.id
+}
+
+
